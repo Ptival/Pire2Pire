b0700800d031022a8432487b0465c55e3c3cca56
Valentin Robert <valentin.robert.42@gmail.com>
2010-12-24 09:23:13 -0800

    Indented my old server.c for fun and profit

--- server.c
+++ server.c
@@ -15,84 +15,84 @@
 int conn = -1, sd = -1;
 
 void kill_handler(int pid) {
-  (void)pid;
-  if(shutdown(sd, SHUT_RDWR) == -1) {
-    perror("shutdown 2");
-  }
-  if(close(sd) == -1) {
-    perror("close 2");
-  }
-  exit(EXIT_SUCCESS);
+    (void)pid;
+    if(shutdown(sd, SHUT_RDWR) == -1) {
+        perror("shutdown 2");
+    }
+    if(close(sd) == -1) {
+        perror("close 2");
+    }
+    exit(EXIT_SUCCESS);
 }
 
 int main(void) {
-  int nbread = -1, nbwritten = -1, retwrite = -1, yes = -1;
-  bool quit = false;
-  struct sockaddr_in sa;
-  char buf[BUF_SIZE];
-  pid_t pid = -1;
+    int nbread = -1, nbwritten = -1, retwrite = -1, yes = -1;
+    bool quit = false;
+    struct sockaddr_in sa;
+    char buf[BUF_SIZE];
+    pid_t pid = -1;
 
-  signal(SIGINT, kill_handler);
+    signal(SIGINT, kill_handler);
 
-  sd = socket(AF_INET, SOCK_STREAM, 0);
-  if(sd == -1) {
-    perror("socket");
-    return EXIT_FAILURE;
-  }
-
-  sa.sin_family = AF_INET;
-  sa.sin_addr.s_addr = INADDR_ANY;
-  sa.sin_port = htons(PORT);
-
-  if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-    perror("setsockopt");
-    exit(EXIT_FAILURE);
-  }
-
-  if(bind(sd, (struct sockaddr *)&sa, sizeof(sa)) == -1) {
-    perror("bind");
-    return EXIT_FAILURE;
-  }
-
-  if(listen(sd, NB_QUEUE)) {
-    perror("listen");
-    return EXIT_FAILURE;
-  }
-
- while(!quit) {
-    conn = accept(sd, NULL, NULL);
-    if(conn == -1) {
-      perror("conn");
-      return EXIT_FAILURE;
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sd == -1) {
+        perror("socket");
+        return EXIT_FAILURE;
     }
 
-    pid = fork();
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = INADDR_ANY;
+    sa.sin_port = htons(PORT);
 
-    switch(pid) {
-    case -1:
-      perror("fork");
-      return EXIT_FAILURE;
-    case 0:
-      do {
-        nbread = read(conn, buf, sizeof(buf));
-        if(nbread == -1) {
-          perror("read");
-          return EXIT_FAILURE;
-        }
-        nbwritten = 0;
-        while(nbwritten < nbread) {
-          retwrite = write(conn, buf + nbwritten, nbread - nbwritten);
-          if(retwrite == -1) {
-            perror("write");
+    if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    if(bind(sd, (struct sockaddr *)&sa, sizeof(sa)) == -1) {
+        perror("bind");
+        return EXIT_FAILURE;
+    }
+
+    if(listen(sd, NB_QUEUE)) {
+        perror("listen");
+        return EXIT_FAILURE;
+    }
+
+    while(!quit) {
+        conn = accept(sd, NULL, NULL);
+        if(conn == -1) {
+            perror("conn");
             return EXIT_FAILURE;
-          }
-          nbwritten += retwrite;
         }
-      } while(nbread > 0);
-    default:
-      break;
-    }
-  }
 
-  return EXIT_SUCCESS;
+        pid = fork();
+
+        switch(pid) {
+            case -1:
+                perror("fork");
+                return EXIT_FAILURE;
+            case 0:
+                do {
+                    nbread = read(conn, buf, sizeof(buf));
+                    if(nbread == -1) {
+                        perror("read");
+                        return EXIT_FAILURE;
+                    }
+                    nbwritten = 0;
+                    while(nbwritten < nbread) {
+                        retwrite = write(conn, buf + nbwritten, nbread - nbwritten);
+                        if(retwrite == -1) {
+                            perror("write");
+                            return EXIT_FAILURE;
+                        }
+                        nbwritten += retwrite;
+                    }
+                } while(nbread > 0);
+            default:
+                break;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
